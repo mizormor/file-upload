fe1b147113bf27227a2cc959207a88cb87cb7e3e
Peter Sowah <petersowah@gmail.com>
2023-06-17 13:47:45 +0000

    clean

--- resources/js/field.js
+++
@@ -1,9 +0,0 @@
-import IndexField from './components/IndexField'
-import DetailField from './components/DetailField'
-import FormField from './components/FormField'
-
-Nova.booting((app, store) => {
-    app.component('index-file-upload', IndexField)
-    app.component('detail-file-upload', DetailField)
-    app.component('form-file-upload', FormField)
-})
